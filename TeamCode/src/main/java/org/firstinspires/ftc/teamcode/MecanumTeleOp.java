fc732ceaa6b5f560663bac77b41f96916c2472be
regressionftc-admin <regressionrobotics@gmail.com>
2025-09-03 12:22:23 -0500

    added servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -1,16 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "MecanumTeleOp")
+
 public class MecanumTeleOp extends LinearOpMode {
+    Limelight3A limelight; //declare name of limelight
     @Override
+
     public void runOpMode() {
         // Declare our motors
         // Make sure your ID's match your configuration
+        //motor declarations
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
@@ -19,12 +26,18 @@ public class MecanumTeleOp extends LinearOpMode {
         DcMotor rightVertSlide = hardwareMap.dcMotor.get("rightVert");
         DcMotor rightHorzSlide = hardwareMap.dcMotor.get("rightHorz");
         DcMotor leftHorzSlide = hardwareMap.dcMotor.get("leftHorz");
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
+        //Reverse everything on the left side
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftVertSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        //init ll3a
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0);
+        //servos
+        Servo transfer = hardwareMap.servo.get("transfer");
+        CRServo intake = hardwareMap.crservo.get("intake");
 
         waitForStart();
 
@@ -40,20 +53,24 @@ public class MecanumTeleOp extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
+            //mecanum code stuff v
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y + x - rx) / denominator;
             double backRightPower = (y - x - rx) / denominator;
-            rightHorzSlide.setPower(ex);
-            leftHorzSlide.setPower(-ex);
-            rightHorzSlide.setPower(-rt);
-            leftHorzSlide.setPower(rt);
-
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+            //vert slide code stuff v
+            rightVertSlide.setPower(ex);
+            leftVertSlide.setPower(ex);
+            rightVertSlide.setPower(rt);
+            leftVertSlide.setPower(rt);
+
+
         }
     }
 }
