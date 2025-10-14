3b19c207f04e40b612b8fd0b6862c705eaaef22e
regressionftc-admin <regressionrobotics@gmail.com>
2025-10-13 22:04:01 -0500

    changes made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -89,7 +89,7 @@ public class MecanumTeleOp extends LinearOpMode {
             }
             if (currentGamepad1.b && !previousGamepad1.b) { //extend slides, flip down transfer, turn on intake
                 gamepad1.rumble(Gamepad.RUMBLE_DURATION_CONTINUOUS);
-                shooter.setPower(1.0);
+                shooter.setPower(0.8);
             }
             if (!currentGamepad1.b && previousGamepad1.b) { //turn off intake, flip up transfer, retract slides
                 shooter.setPower(0);
